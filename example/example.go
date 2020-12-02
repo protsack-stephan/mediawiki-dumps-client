4c753341d3aabe68cab1fcb3cc1d684df2c70328
Stephan Protsack <stephan@speedandfunction.com>
2020-12-02 16:18:52 +0200

    Updated the example

--- example/example.go
+++ example/example.go
@@ -3,20 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
-	"net/http"
 	"time"
 
 	dumps "github.com/protsack-stephan/mediawiki-dumps-client"
 )
 
 func main() {
-	client := dumps.NewBuilder().
-		URL("http://new-url.com").
-		HTTPClient(&http.Client{}).
-		Options(&dumps.Options{}).
-		Build()
-
-	// client := dumps.NewCLient()
+	client := dumps.NewClient()
 
 	titles, err := client.PageTitles(context.Background(), "enwikinews", time.Now().UTC())
 
