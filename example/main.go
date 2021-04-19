c9399d951eeedd104adcbeed0da40a26ee093d1f
Stephan Protsack <stephan@speedandfunction.com>
2021-04-19 22:32:14 +0300

    Example update

--- example/main.go
+++ example/main.go
@@ -12,14 +12,13 @@ import (
 func main() {
 	client := dumps.NewClient()
 
-	// _ = client.PageTitles(context.Background(), "enwikinews", time.Now().UTC(), func(p *dumps.Page) {
-	// 	fmt.Println(p)
-	// })
+	_ = client.PageTitles(context.Background(), "enwikinews", time.Now().UTC(), func(p *dumps.Page) {
+		fmt.Println(p)
+	})
 
 	date := time.Date(time.Now().Year(), time.Now().Month(), 1, 0, 0, 0, 0, time.UTC)
 
 	ns, _ := client.Namespaces(context.Background(), "ukwikinews", date)
-
 	fmt.Println(ns)
 
 	err := client.PageTitlesNs(context.Background(), "ukwikinews", date, func(p *dumps.Page) {
